7081955c791af70f162a6d9a8fa55e4947d97401
UnioTrump <out_czh_001@outlook.com>
2025-07-15 00:41:11 +0800

    v2.0

--- demo.py
+++ demo.py
@@ -4,28 +4,38 @@ import torch
 import os
 import time
 import numpy as np
-from utils import ProteinData, PPIDataLoader, WeightedCrossEntropy, calculate_metrics
+from utils import WeightedCrossEntropy, calculate_metrics
 from utils import find_best_threshold_by_f_beta, find_best_threshold_by_mcc
 from GASPPI import HierarchicalGNN
 # from config import DefaultConfig
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 torch.manual_seed(123)
 
+'''
 def load_data(pkl_path):
-    try:
-        with open(pkl_path, 'rb') as f:
-            raw_data = pickle.load(f)
-        print(f"Loaded data from {pkl_path}")
-        return raw_data
-    except Exception as e:
-        print(f"Error loading data from {pkl_path}: {e}")
-        raise
+    with open(pkl_path, 'rb') as f:
+        raw_data = pickle.load(f)
+    print(f"Loaded data from {pkl_path}")
+    return raw_data
+'''
+def load_data(pkl_path):
+    with open(pkl_path, 'rb') as f:
+        train_list = pickle.load(f)
+    return train_list
 
-raw_dict = load_data('/gz-data/train355-r5.5-a2.3.pkl')
+train_list = load_data('/gz-data/train355-r5.5-a2.3.pkl')
 
 # 创建完整数据集
-full_dataset = ProteinData(raw_dict)
-
+'''
+full_dataset = ProteinData(train_list)
+'''
+samples_num = len(train_list)
+split_num = int(0.8 * samples_num)
+data_index = train_list
+np.random.shuffle(data_index)
+train_data = data_index[:split_num]
+val_data = data_index[split_num:]
+'''
 # 划分数据集
 def split_dataset(dataset, train_ratio=0.8, val_ratio=0.2, random_seed=123):
     """将数据集分割为训练集、验证集"""
@@ -51,8 +61,8 @@ def split_dataset(dataset, train_ratio=0.8, val_ratio=0.2, random_seed=123):
     # 创建子数据集
     train_dataset = [dataset[i] for i in train_indices]
     val_dataset = [dataset[i] for i in val_indices]
-    
-    # 创建新的数据集实例
+
+# 创建新的数据集实例
     train_set = ProteinData.__new__(ProteinData)
     train_set.samples = train_dataset
     
@@ -65,12 +75,17 @@ def split_dataset(dataset, train_ratio=0.8, val_ratio=0.2, random_seed=123):
 train_dataset, val_dataset = split_dataset(full_dataset)
 
 # 创建数据加载器
-train_loader = PPIDataLoader(train_dataset, batch_size=16, shuffle=True)
-val_loader = PPIDataLoader(val_dataset, batch_size=16, shuffle=True)
-
+train_loader = PPIDataLoader(train_data, batch_size=16, shuffle=True)
+val_loader = PPIDataLoader(val_data, batch_size=16, shuffle=True)
+'''
 # 计算数据集中原子和残基的最大数量
-max_atom_nodes = max([sample['atom_graph'].x.size(0) for sample in full_dataset.samples])       # 13512
-max_residue_nodes = max([sample['residue_graph'].x.size(0) for sample in full_dataset.samples]) # 869
+atom_nodes = []     # 13512
+residue_nodes = [] # 869
+for _,data_dict in enumerate(train_list):
+    atom_nodes.append(len(data_dict['atom_graph_node']))
+    residue_nodes.append(len(data_dict['residue_graph_node']))
+max_atom_nodes = max(atom_nodes)
+max_residue_nodes = max(residue_nodes)
 
 model = HierarchicalGNN(
     atom_num_nodes=max_atom_nodes,
@@ -91,18 +106,22 @@ model = HierarchicalGNN(
 ).to(device)
 
 POS_WEIGHT = 1 / torch.tensor(np.sqrt(5.39734))  # 定义全局正样本权重，提升稳定性.这是全部样本的正负样本比值
-def train(run, model, loader, optimizer, grad_norm=None, delta=0.5):
+def train(run, model, train_proteins, optimizer, grad_norm=None, delta=0.5):
     model.train()
 
-    total_loss = total_examples = 0
+    b_loss = 0
+    e_loss = 0
     '''还未使用POS_WEIGHT'''
     criterion = WeightedCrossEntropy(pos_wt=1/POS_WEIGHT, device=device)
     # 移除内部进度条
-    for batch in loader:
-        for k, v in batch.items():
-            if isinstance(v, torch.Tensor):
-                batch[k] = v.to(device)
-
+    for train_p in train_proteins:
+        train_p_a_node = torch.FloatTensor(train_p['atom_graph_node']).cuda()
+        train_p_a_edge = torch.LongTensor(train_p['atom_graph_edge']).cuda()
+        train_p_r_node = torch.FloatTensor(train_p['residue_graph_node']).cuda()
+        train_p_r_edge = torch.LongTensor(train_p['residue_graph_edge']).cuda()
+        targets = torch.LongTensor(train_p['label']).cuda()
+        a2r_map = torch.tensor(train_p['a2r_map']).cuda()
+        '''
         mask = batch['train_mask']
         if mask.sum() == 0:
             continue
@@ -115,56 +134,52 @@ def train(run, model, loader, optimizer, grad_norm=None, delta=0.5):
         residue_adj_t = batch['residue_adj_t']
         a2r_map = batch['a2r_map']
         targets = batch['y']
-
+        '''
         # 前向传播
-        out = model(atom_x, atom_adj_t, residue_x, residue_adj_t, a2r_map)
-
+        optimizer.zero_grad()
+        out = model(train_p_a_node, train_p_a_edge, train_p_r_node, train_p_r_edge, a2r_map)
         loss = criterion.compute_loss(out, targets)
         loss.backward()
         if grad_norm is not None:
             torch.nn.utils.clip_grad_norm_(model.parameters(), grad_norm)
         optimizer.step()
 
+        b_loss = loss.item()
+        e_loss += b_loss
+        e_loss /= len(train_proteins)
+        '''
         batch_loss = float(loss) * mask.sum().item()
         total_loss += batch_loss
         total_examples += mask.sum().item()
-
-    return total_loss / total_examples
+        '''
+    return e_loss
 
 @torch.no_grad()
-def test(model, loader):
+def test(model, val_proteins):
     """评估模型在给定数据集上的性能"""
     model.eval()
     criterion = WeightedCrossEntropy(pos_wt=1/POS_WEIGHT, device=device)
-    total_loss = total_examples = 0
+    b_loss = e_loss = 0
     all_probs, all_targets = [], []
 
-    for batch in loader:
-        for k, v in batch.items():
-            if isinstance(v, torch.Tensor):
-                batch[k] = v.to(device)
-        
-        mask = batch['train_mask']
-        if mask.sum() == 0:
-            continue
-        
-        # 提取数据
-        atom_x = batch['atom_x']
-        atom_adj_t = batch['atom_adj_t']
-        residue_x = batch['residue_x']
-        residue_adj_t = batch['residue_adj_t']
-        a2r_map = batch['a2r_map']
-        targets = batch['y']
+    for val_p in val_proteins:
+        val_p_a_node = torch.FloatTensor(val_p['atom_graph_node']).cuda()
+        val_p_a_edge = torch.LongTensor(val_p['atom_graph_edge']).cuda()
+        val_p_r_node = torch.FloatTensor(val_p['residue_graph_node']).cuda()
+        val_p_r_edge = torch.LongTensor(val_p['residue_graph_edge']).cuda()
+        targets = torch.LongTensor(val_p['label']).cuda()
+        a2r_map = torch.tensor(val_p['a2r_map']).cuda()
 
         # 前向传播
-        out = model(atom_x, atom_adj_t, residue_x, residue_adj_t, a2r_map)
+        optimizer.zero_grad()
+        out = model(val_p_a_node, val_p_a_edge, val_p_r_node, val_p_r_edge, a2r_map)
 
         # 计算损失
         loss = criterion.compute_loss(out, targets)
 
-        batch_loss = float(loss) * mask.sum().item()
-        total_loss += batch_loss
-        total_examples += mask.sum().item()
+        b_loss = loss.item()
+        e_loss += b_loss
+        e_loss /= len(val_proteins)
 
         # 记录batch标签和预测值
         all_probs.append(torch.sigmoid(out))
@@ -178,7 +193,7 @@ def test(model, loader):
         torch.cat(all_probs, dim=0),
         threshold)
 
-    return total_loss / total_examples, metrics, threshold
+    return e_loss, metrics, threshold
 
 def save_checkpoint(model, optimizer, epoch, loss, val_loss, filename):
     """保存模型检查点"""
@@ -204,14 +219,18 @@ optimizer = torch.optim.SGD(model.parameters(), lr=1e-3, momentum=0.9, weight_de
 # 使用tqdm创建整体训练进度条
 print("开始训练...")
 epoch_pbar = tqdm(range(num_epochs), desc="训练进度", ncols=160)
+train_losses = []
+val_losses = []
 for epoch in epoch_pbar:
     # 训练
     start_time = time.time()
-    train_loss = train(0, model, train_loader, optimizer, grad_norm=1.0)
+    train_loss = train(0, model, train_data, optimizer, grad_norm=1.0)
+    train_losses.append(train_loss)
     train_time = time.time() - start_time
 
     # 在验证集上评估
-    val_loss, metrics, best_threshold = test(model, val_loader)
+    val_loss, metrics, best_threshold = test(model, val_data)
+    val_losses.append(val_loss)
 
     # 更新进度条
     epoch_pbar.set_postfix({
@@ -224,7 +243,7 @@ for epoch in epoch_pbar:
     })
     epoch_pbar.update(1)
     # 保存最佳准确率模型
-    if val_loss < best_loss:
+    if val_losses[-1] < best_loss:
         best_loss = val_loss
         save_checkpoint(
             model, optimizer, epoch, train_loss, val_loss,
