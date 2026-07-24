81321c3bc03620cd1263dde964858f41a503e7c0
UnioTrump <out_czh_001@outlook.com>
2026-07-24 15:17:06 +0800

    Optimized the handling of the dataset and addressed some minor issues.

--- run.sh
+++ run.sh
@@ -2,4 +2,16 @@ echo 'Pretrain on Train2066 dataset'
 python demo.py
 
 echo 'Test on Test60 dataset'
-python Val.py
+python Val.py --data_path ../gz-data/Test_60
+
+echo 'Test on Test315 dataset'
+python Val.py --data_path ../gz-data/Test_315
+
+echo 'Test on DSet_72 dataset'
+python Val.py --data_path ../gz-data/Test_72
+
+echo 'Test on Test164 dataset'
+python Val.py --data_path ../gz-data/Test_164
+
+echo 'Test on Test186 dataset'
+python Val.py --data_path ../gz-data/Test_186
